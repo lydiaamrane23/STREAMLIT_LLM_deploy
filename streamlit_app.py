90df2ec1b1c8ca508803e7567ca4b14f1d1febec
Lydia_AMRANE <amrane.lidia@gmail.com>
2023-12-01 18:06:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,94 +1,41 @@
-import orchest
 import streamlit as st
+import openai
+import os
+#from openai import AsyncOpenAI
+from openai import OpenAI
+
+st.title("Artelia LLM Chat v02")
+
+openai.api_key = "vide"
+os.environ['OPENAI_API_KEY'] = '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'
+openai.api_base = os.getenv("OPENAI_API_BASE")
+
+# Set a default model
+if "model" not in st.session_state:
+    st.session_state["model"] = os.getenv("MODEL")
 
 
-import orchest
-import weaviate
 
-
-def get_weaviate_client():
-   
-    auth_config = weaviate.AuthApiKey(api_key="test")
-    return weaviate.Client(
-   url = "http://weaviate:8080",  # Replace with your endpoint
-   auth_client_secret=auth_config,
-   additional_headers = {
-         "X-OpenAI-Api-Key": "test"  # Replace with your inference API key
-     }
- )
-
-
-def define_schema():
-
-    client = get_weaviate_client()
-    client.schema.delete_all()
-    client.schema.get()  # get the full schema as example
-
-    schema = {
-        "classes": [
-            {
-                "class": "Author",
-                "description": "The writer of a comments",
-                "properties": [
-                    {
-                        "dataType": ["string"],
-                        "description": "Name of the author",
-                        "name": "name",
-                    },
-                    {
-                        "dataType": ["Comment"],
-                        "description": "Comments this author wrote",
-                        "name": "writtenComments",
-                    },
-                ],
-            },
-            {
-                "class": "Comment",
-                "description": "A written comment, by an author",
-                "properties": [
-                    {
-                        "dataType": ["text"],
-                        "description": "The content of the comment",
-                        "name": "content",
-                    },
-                    {
-                        "dataType": ["text"],
-                        "description": "Permalink to the comment.",
-                        "name": "permalink",
-                    },
-                    {
-                        "dataType": ["date"],
-                        "description": "The date of when the comment was written",
-                        "name": "date",
-                    },
-                ],
-            },
-        ]
-    }
-
-    client.schema.create(schema)
-client = get_weaviate_client()
-
-query = st.text_input("Zoek door comments", key="query")
-
-if len(query) > 0:
-    content = {
-        "concepts": [query],
-    }
-
-    result = (
-        client.query.get("Comment", ["content", "date", "permalink"])
-        .with_near_text(content)
-        .with_limit(10)
-        .do()
-    )
-
-    comments = result["data"]["Get"]["Comment"]
-
-    if comments is None:
-        st.write("Did not find any comments.")
-    else:
-        for comment in comments:
-            st.markdown("[" + comment["date"] + "]("+comment["permalink"]+")")
-            st.markdown(comment["content"])
-            st.markdown("---")
+# Accept user input
+if prompt := st.chat_input("What's up at Artelia?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in OpenAI().chat.completions.create(
+            model=st.session_state["model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
