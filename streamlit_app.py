90642b8f0220f33db19d6f0d07d3a58880243e4e
Lydia_AMRANE <amrane.lidia@gmail.com>
2023-12-01 14:19:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ with st.form('my_form'):
     submitted = st.form_submit_button('Submit')
 
     if submitted and openai_api_key.startswith('sk-'):
-         st.info(text)
+        st.info(text)
         docs = vectorstore.similarity_search(text)
         st.info("ok",docs)
         
