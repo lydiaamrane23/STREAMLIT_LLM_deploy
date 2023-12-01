d7f1a9a56e85ec97e1a53d81bc6e548d4ca12a2e
Lydia_AMRANE <amrane.lidia@gmail.com>
2023-12-01 13:07:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,69 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
+import weaviate
+
+
+
+## replace EMBEDDEDOPTION BY THIS WHEN i Wwill have information
+#auth_config = weaviate.AuthApiKey(api_key="test")
+#url = "http://weaviate:8080",  # Replace with your endpoint
+#auth_client_secret=auth_config,
+
+client = weaviate.Client(
+    embedded_options=weaviate.EmbeddedOptions(),
+    additional_headers={
+        "X-OpenAI-Api-Key": openai_key
+    }
+)
+rom langchain.vectorstores import Weaviate
+
+
+
+# define input structure
+client.schema.delete_all()
+client.schema.get()
+schema = {
+    "classes": [
+        {
+            "class": "Chatbot",
+            "description": "Documents for chatbot",
+            "vectorizer": "text2vec-openai",
+            "moduleConfig": {"text2vec-openai": {"model": "ada", "type": "text"}},
+            "properties": [
+                {
+                    "dataType": ["text"],
+                    "description": "The content of the paragraph",
+                    "moduleConfig": {
+                        "text2vec-openai": {
+                            "skip": False,
+                            "vectorizePropertyName": False,
+                        }
+                    },
+                    "name": "content",
+                },
+            ],
+        },
+    ]
+}
+
+client.schema.create(schema)
+vectorstore = Weaviate(client, "Chatbot", "content", attributes=["source"])
+
 st.title('🦜🔗 Quickstart App')
 
 openai_api_key ="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" 
 
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info("ok")
+    # retrieve text related to the query
+    docs = vectorstore.similarity_search(query)
+    st.info("ok",docs)
 
 with st.form('my_form'):
     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
     submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
+
     if submitted and openai_api_key.startswith('sk-'):
         generate_response(text)
 
