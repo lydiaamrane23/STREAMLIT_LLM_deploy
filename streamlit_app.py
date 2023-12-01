f618c5b9ff1c12debd83f98bee7c4e6079943bef
Lydia_AMRANE <amrane.lidia@gmail.com>
2023-12-01 17:39:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,89 @@
+import orchest
 import streamlit as st
-import openai
-import os
-#from openai import AsyncOpenAI
-from openai import OpenAI
 
-st.title("Artelia LLM Chat v02")
 
-openai.api_key = "vide"
-os.environ['OPENAI_API_KEY'] = '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'
-openai.api_base = os.getenv("OPENAI_API_BASE")
+import orchest
+import weaviate
 
-# Set a default model
-if "model" not in st.session_state:
-    st.session_state["model"] = os.getenv("MODEL")
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+def get_weaviate_client():
+    weaviate_service = orchest.get_service("weaviate")
+    return weaviate.Client(
+        f"http://{weaviate_service['internal_hostname']}:{weaviate_service['ports'][0]}"
+    )
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
 
-# Accept user input
-if prompt := st.chat_input("What's up at Artelia?"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-       
+def define_schema():
 
-    st.session_state.messages.append({"role": "assistant", "content": "kaka"})
+    client = get_weaviate_client()
+    client.schema.delete_all()
+    client.schema.get()  # get the full schema as example
+
+    schema = {
+        "classes": [
+            {
+                "class": "Author",
+                "description": "The writer of a comments",
+                "properties": [
+                    {
+                        "dataType": ["string"],
+                        "description": "Name of the author",
+                        "name": "name",
+                    },
+                    {
+                        "dataType": ["Comment"],
+                        "description": "Comments this author wrote",
+                        "name": "writtenComments",
+                    },
+                ],
+            },
+            {
+                "class": "Comment",
+                "description": "A written comment, by an author",
+                "properties": [
+                    {
+                        "dataType": ["text"],
+                        "description": "The content of the comment",
+                        "name": "content",
+                    },
+                    {
+                        "dataType": ["text"],
+                        "description": "Permalink to the comment.",
+                        "name": "permalink",
+                    },
+                    {
+                        "dataType": ["date"],
+                        "description": "The date of when the comment was written",
+                        "name": "date",
+                    },
+                ],
+            },
+        ]
+    }
+
+    client.schema.create(schema)
+client = get_weaviate_client()
+
+query = st.text_input("Zoek door comments", key="query")
+
+if len(query) > 0:
+    content = {
+        "concepts": [query],
+    }
+
+    result = (
+        client.query.get("Comment", ["content", "date", "permalink"])
+        .with_near_text(content)
+        .with_limit(10)
+        .do()
+    )
+
+    comments = result["data"]["Get"]["Comment"]
+
+    if comments is None:
+        st.write("Did not find any comments.")
+    else:
+        for comment in comments:
+            st.markdown("[" + comment["date"] + "]("+comment["permalink"]+")")
+            st.markdown(comment["content"])
+            st.markdown("---")
