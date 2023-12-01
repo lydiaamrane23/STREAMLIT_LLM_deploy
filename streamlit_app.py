9d9cf13ee65ad10bae51261fcd29fda7442a043a
Lydia_AMRANE <amrane.lidia@gmail.com>
2023-12-01 12:08:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 
 import streamlit as st
 import numpy as np
+import weaviate
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+openai_key = "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"
+
+client = weaviate.Client(
+    embedded_options=weaviate.EmbeddedOptions(),
+    additional_headers={
+        "X-OpenAI-Api-Key": openai_key
+    }
+)
+vectorstore = Weaviate(client, "Chatbot", "content", attributes=["source"])
+
 
 
 # Write directly to the app
@@ -14,4 +27,15 @@ selected_category = st.sidebar.selectbox("Mission category", ["All","Contrats","
 with st.form('my_form'):
   query_text = st.text_input('Enter your question :', '')
   submitted = st.form_submit_button('Submit')
+# retrieve text related to the query
+docs = vectorstore.similarity_search(query_text)
+if submitted:
+  chain = load_qa_chain(
+    OpenAI(openai_api_key ="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",temperature=0), 
+    chain_type="stuff")
+  # create answer
+  st.info(chain.run(input_documents=docs, question=query))
+  
+
+
 
