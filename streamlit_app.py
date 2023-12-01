7dc68bf30fc293e37acac2cda3d82abc72ace8c9
Lydia_AMRANE <amrane.lidia@gmail.com>
2023-12-01 12:52:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,20 @@
-
 import streamlit as st
-import numpy as np
-import weaviate
-from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-openai_key = "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"
 
-client = weaviate.Client(
-    embedded_options=weaviate.EmbeddedOptions(),
-    additional_headers={
-        "X-OpenAI-Api-Key": openai_key
-    }
-)
-vectorstore = Weaviate(client, "Chatbot", "content", attributes=["source"])
+st.title('🦜🔗 Quickstart App')
 
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
 
-# Write directly to the app
-st.title("ASK THE DOC APP")
-st.sidebar.header("Specify ATTRIBUT")
-selected_number = st.sidebar.text_input("Mission Number")
-selected_type = st.sidebar.text_input("Mission type")
-selected_location = st.sidebar.text_input("Mission location")
-selected_client = st.sidebar.text_input("Client")
-selected_category = st.sidebar.selectbox("Mission category", ["All","Contrats","CR","Cahier de charge","Devis/offres","Factures"])
 with st.form('my_form'):
-  query_text = st.text_input('Enter your question :', '')
-  submitted = st.form_submit_button('Submit')
-# retrieve text related to the query
-docs = vectorstore.similarity_search(query_text)
-if submitted:
-  chain = load_qa_chain(
-    OpenAI(openai_api_key ="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",temperature=0), 
-    chain_type="stuff")
-  # create answer
-  st.info(chain.run(input_documents=docs, question=query))
-  
-
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
 
 
